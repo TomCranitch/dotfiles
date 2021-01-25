f4bccbdd6df420146295c8666683501e3d1dbc55
Tom Cranitch <tom@cranitch.com.au>
2021-01-25 12:00:54 +1000

    Changes

--- .scripts/lg-display.sh
+++ .scripts/lg-display.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if xrandr | grep "DP1-2 connected"; then
-	xrandr --output eDP1 --off --output DP1-2 --auto
+if xrandr | grep "DP-1-2 connected"; then
+	xrandr --output eDP-1 --off --output DP-1-2 --auto
 	killall -q compton
 	while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
 	compton -b &
 	killall -q polybar
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-	polybar white-second &
+	polybar bar &
 	wal -R &
 fi
--- .zshrc
+++ .zshrc
@@ -73,7 +73,7 @@ export PATH=/opt/flutter/bin:$PATH
 export OPENCV_LOG_LEVEL=ERROR
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
+# export JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Add .NET Core SDK tools
